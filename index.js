b2d25f39511da3eb856d4516aebc7d2dc5283b69
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2022-12-17 03:40:48 +0530

    up1

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ try {
     dotenv.config({ path: './config.env' });
     const port = process.env.PORT || 3000;
     const mongoURI = process.env.MONGO || "mongodb+srv://tushar:Bf9Z7Lgqw8xcTA0x@cluster0.3zsfvdw.mongodb.net/banao";
-    
+
     app.use(express.static(path.join(__dirname, './client/build')))
 
     connectToDb();
     app.use(express.json())
 
+    app.get('/', (req, res) => {
+        res.status(404).send("Forbidden");
+    })
+    
     app.use('/api/auth', require('./routes/auth'));
     app.use('/api/data', require('./routes/data'));
 
